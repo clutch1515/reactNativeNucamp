2b3114fd910cecbc86cf045fb713e39cd782548c
Peter Larsen <clutch1515@gmail.com>
2020-04-22 11:27:31 -0700

    Week 1

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Main from './components/MainComponent';
+
+export default function App() {
+    return (
+        <Main />
+    );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
